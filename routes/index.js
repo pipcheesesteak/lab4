6399ecc568d233b9b12dff77d029095aef4724ed
Phillip Zhou <pzhou53@gmail.com>
2016-10-27 19:33:36 -0700

    adsfa

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
+/* Load the home page. */
 
-/*
- * GET home page.
- */
+exports.view = function (req, res) {
+    var Aspects = {
+        // Top row - contains emotion tracker and conversation ideas aspects.
+        "AspectTopColumns": [
+            {
+                "ImageHolder": "images/ImageHolder.png",
+                "TextBox": "images/EmotionTracker.png",
+                "Image": "images/Emotion.png"
+            },
+            {
+                "ImageHolder": "images/ImageHolder.png",
+                "TextBox": "images/ConversationIdeas.png",
+                "Image": "images/Conversation.png"
+            }
+        ],
+        // Bottom row - contains relationship profile and switch profile aspects.
+        "AspectBotColumns": [
+            {
+                "ImageHolder": "images/ImageHolder.png",
+                "TextBox": "images/RelationshipProfile.png",
+                "Image": "images/Relationship.png"
+            },
+            {
+                "ImageHolder": "images/ImageHolder.png",
+                "TextBox": "images/SwitchProfile.png",
+                "Image": "images/Switch.png"
+            }
+        ],
+        "HomeButton": "images/HomeSymbol.png"
+    };
 
-exports.view = function(req, res){
-  res.render('index');
+    // Send this data to Home.handlebars.
+    res.render("index", Aspects);
 };
